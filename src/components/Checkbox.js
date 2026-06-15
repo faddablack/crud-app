82ffed6b1ff6e39109cce1d63402eaa9548e5222
Isaac <1440isaack@gmail.com>
2026-06-15 19:12:25 +0300

    Added EditForm AddTodoForm TodoItem

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-// Destructuring props directly in the arguments
 function Checkbox({ isChecked, onChange }) {
   return (
     <input 
       type="checkbox" 
       checked={isChecked} 
-      onChange={(e) => onChange(e.target.checked)} 
+      onChange={(e) => alert(e.target.checked)} 
       style={{ cursor: 'pointer', width: '20px', height: '20px' }}
     />
   );
